517fccd26b706385b63d02fc273f012207fe1d8d
Alexandru Savin <alexandrusavin@users.noreply.github.com>
2019-09-25 18:14:50 +0200

    Create type script definitions

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import * as nock from 'nock';
+
+declare global {
+
+  export namespace Chai {
+
+    interface LanguageChains {
+      always: Assertion;
+    }
+
+    interface Assertion {
+      /**
+       * true if the spy was called at least once.
+       */
+      requested: Assertion;
+      /**
+       * @param count The number of recorded calls.
+       */
+      requestedWith(body: any): Assertion;
+      /**
+       * true if the spy was called exactly once.
+       */
+      requestedWithHeaders(headers: object): Assertion;
+      /**
+       * true if the spy was called exactly twice.
+       */
+      requestedWithHeadersMatch(headers: object): Assertion;
+    }
+  }
+}
+
+declare var chaiNock: {(chai: any, utils: any): void, setTimeout: (timeout: number) => void};
+declare namespace chaiNock { }
+export = chaiNock;
